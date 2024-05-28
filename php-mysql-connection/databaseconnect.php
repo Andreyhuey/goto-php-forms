380d3ece312597833f143d48be746e0914786e7b
Andreyhuey <andreyhuey777@gmail.com>
2024-05-28 09:46:32 +0100

    feat: databaseconnect.php added

---
+++ php-mysql-connection/databaseconnect.php
@@ -0,0 +1,23 @@
+<!-- https://www.hostinger.com/tutorials/how-to-connect-php-to-mysql -->
+
+<?php
+$servername = "localhost";
+$database = "u123456789_DatabaseName";
+$username = "u123456789_User";
+$password = "MyStr0ngPa$!";
+ 
+// Create connection
+ 
+$conn = mysqli_connect($servername, $username, $password, $database);
+ 
+// Check connection
+ 
+if (!$conn) {
+ 
+    die("Connection failed: " . mysqli_connect_error());
+ 
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
+
